ed3aea5625cd9d841adc9a890e6041d2f17edbe4
CrMariana <cristobalmariana2302@gmail.com>
2023-09-24 18:09:38 -0500

    registro-usuario

--- GF_APP_FRONT/src/app/registro-usuario/registro-usuario.page.ts
+++ GF_APP_FRONT/src/app/registro-usuario/registro-usuario.page.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro-usuario',
   templateUrl: './registro-usuario.page.html',
   styleUrls: ['./registro-usuario.page.scss'],
 })
-export class RegistroUsuarioPage implements OnInit {
+export class RegistroUsuarioPage {
+  dni: string = '';
+  nombres: string = '';
+  // Agrega otras propiedades para los demás campos
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
-  ngOnInit() {
+  async registrar() {
+    // Verifica si todos los campos requeridos están llenos
+    if (!this.dni || !this.nombres /* Agrega aquí las demás propiedades */) {
+      await this.mostrarAlertaCamposVacios();
+    } else {
+      // Continúa con el proceso de registro si todos los campos están llenos
+      // ...
+    }
   }
 
+  async mostrarAlertaCamposVacios() {
+    const alert = await this.alertController.create({
+      header: 'Campos Vacíos',
+      message: 'Por favor, llena todos los campos.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 }
